be18d542e2a73a4e135cb7e850d2a90160cde183
LeRobinet <salopst>
2021-10-29 17:07:06 +0100

    Re-added search

--- assets/js/main.js
+++ assets/js/main.js
@@ -23,6 +23,32 @@ function detectOSColorTheme() {
   }
 }
 
+
+//determines if the user has a set theme
+function detectUserColorScheme(){
+  var theme="light";    //default to light
+
+  //local storage is used to override OS theme settings
+  if(localStorage.getItem("theme")){
+      if(localStorage.getItem("theme") == "dark"){
+          var theme = "dark";
+      }
+  } else if(!window.matchMedia) {
+      //matchMedia method not supported
+      return false;
+  } else if(window.matchMedia("(prefers-color-scheme: dark)").matches) {
+      //OS theme setting detected as dark
+      var theme = "dark";
+  }
+
+  //dark theme preferred, set document with a `data-theme` attribute
+  if (theme=="dark") {
+       document.documentElement.setAttribute("data-theme", "dark");
+  }
+}
+detectUserColorScheme();
+
+
 // Switch the theme.
 function switchTheme(e) {
   if (chosenThemeIsDark) {
@@ -32,6 +58,7 @@ function switchTheme(e) {
   }
 
   detectOSColorTheme();
+  detectUserColorScheme();
   window.location.reload();
 }
 
